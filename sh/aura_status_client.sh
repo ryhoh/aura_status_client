b21bb2ba90fab4d674262e4132c0de56e75661a7
ryhoh <axis38akasira@gmail.com>
2020-09-02 17:24:47 +0900

    fix

--- sh/aura_status_client.sh
+++ sh/aura_status_client.sh
@@ -4,6 +4,6 @@ NAME='client_name'
 
 while true
 do
-   curl -X POST -d 'name=$NAME' https://aurastatusboard.herokuapp.com/api/heartbeat
+   curl -X POST -d "name=$NAME" https://aurastatusboard.herokuapp.com/api/heartbeat
    sleep 900
 done
