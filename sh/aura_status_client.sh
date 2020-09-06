89575203b475f91128a9aa60d108101d45939b5a
ryhoh <axis38akasira@gmail.com>
2020-09-06 18:32:00 +0900

    Update aura_status_client.sh
    add deep sleep mode

--- sh/aura_status_client.sh
+++ sh/aura_status_client.sh
@@ -6,4 +6,12 @@ while true
 do
    curl -X POST -d "name=$NAME" https://aurastatusboard.herokuapp.com/api/heartbeat
    sleep 900
+    # Hour [0, 6]: take deep sleep until 6.
+    hour=`date "+%H"`
+    if [ $hour -lt 6 ]
+    then
+        sleep $(( (6 - hour) * 3600 ))
+    else  # 15 min sleep
+        sleep 900
+    fi
 done
